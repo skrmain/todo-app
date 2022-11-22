310cb0e9199dbec966a0866b99cf6ec5c78eae72
Sachin Kumar Rajput <31756244+skrmain@users.noreply.github.com>
2022-11-22 16:35:15 +0000

    [feat] add todoForm, todoList(json)
    - Integrate with API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,55 +1,42 @@
 import { Component } from '@angular/core';
-import { ApiServiceService } from './services/api-service.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { ApiServiceService } from './services/todo-api-service.service';
+import { TodoType } from './types/todo.types';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>Welcome to {{ title }}!</h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img
-        width="300"
-        alt="Angular Logo"
-        src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg=="
-      />
-    </div>
-    <h2>Here are some links to help you start:</h2>
-    <ul>
-      <li>
-        <h2>
-          <a target="_blank" rel="noopener" href="https://angular.io/tutorial"
-            >Tour of Heroes</a
-          >
-        </h2>
-      </li>
-      <li>
-        <h2>
-          <a target="_blank" rel="noopener" href="https://angular.io/cli"
-            >CLI Documentation</a
-          >
-        </h2>
-      </li>
-      <li>
-        <h2>
-          <a target="_blank" rel="noopener" href="https://blog.angular.io/"
-            >Angular blog</a
-          >
-        </h2>
-      </li>
-    </ul>
-    <router-outlet></router-outlet>
-  `,
+  templateUrl: './app.component.html',
   styles: [],
 })
 export class AppComponent {
   title = 'website';
+  todos: TodoType[] = [];
+  todoForm = new FormGroup({
+    title: new FormControl('', Validators.required),
+    detail: new FormControl(''),
+  });
 
   constructor(private apiService: ApiServiceService) {}
 
   ngOnInit() {
-    this.apiService.get().subscribe((data) => {
+    this.apiService.getTodos().subscribe((data: any) => {
       console.log('[data]', data);
+      this.todos = data.data;
     });
+  }
+
+  addTodo() {
+    console.log('[form-data]', this.todoForm.value);
+    this.apiService
+      .addTodo({
+        title: this.todoForm.get('title')?.value || '',
+        detail: this.todoForm.get('detail')?.value || '',
+      })
+      .subscribe((data) => {
+        console.log('[data] ', data);
+        this.ngOnInit();
+        this.todoForm.reset();
+      });
   }
 }
--- src/app/services/todo-api-service.service.ts
+++ src/app/services/todo-api-service.service.ts
@@ -1,17 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { TodoType } from '../types/todo.types';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiServiceService {
-  baseUrl: string;
+  private baseUrl: string;
   constructor(private http: HttpClient) {
     this.baseUrl = environment.baseUrl;
   }
 
-  get() {
-    return this.http.get(this.baseUrl);
+  getTodos() {
+    return this.http.get(this.baseUrl + '/todo');
+  }
+
+  addTodo(todo: TodoType) {
+    return this.http.post(this.baseUrl + '/todo', { ...todo });
   }
 }
